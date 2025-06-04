cf212e8b053a86dbb8835613eb3d1bcf3a177d06
łukasz :3 <lukasz-strama@outlook.com>
2025-06-04 12:59:39 +0200

    update dice_logic.py

--- dice_logic.py
+++ dice_logic.py
@@ -2,20 +2,22 @@ import random
 from math import comb
 
 def roll_dice(num_dice):
-    """Roll num_dice d6 dice and return the results"""
+    """Roll num_dice 6-sided dice and return the results as a list."""
     return [random.randint(1, 6) for _ in range(num_dice)]
 
 def count_successes(dice_results):
-    """Count the number of 6s (successes) in the dice results"""
-    return sum(1 for die in dice_results if die == 6)
+    """Count how many dice rolled a 6 (i.e., count successes)."""
+    return dice_results.count(6)
 
 def calculate_probability(num_dice, successes):
-    """Calculate the probability of getting exactly 'successes' successes with 'num_dice' dice"""
-    # Probability of success on a single die (rolling a 6)
-    p_success = 1/6
-    # Probability of failure on a single die (rolling 1-5)
-    p_failure = 5/6
-    
-    # Binomial probability formula
-    probability = comb(num_dice, successes) * (p_success ** successes) * (p_failure ** (num_dice - successes))
+    """
+    Calculate the binomial probability of rolling exactly 'successes'
+    sixes with 'num_dice' 6-sided dice.
+    """
+    if successes > num_dice or successes < 0:
+        return 0.0
+
+    p = 1 / 6
+    q = 5 / 6
+    probability = comb(num_dice, successes) * (p ** successes) * (q ** (num_dice - successes))
     return probability * 100
